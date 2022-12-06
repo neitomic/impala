bfc0d7c0c4760d3deab4f5afa7b6d4a52f073fe0
Tien Nguyen <thanhtien522@gmail.com>
2022-12-06 22:10:13 +0700

    add cdh build scripts

---
+++ docker/cdh/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+IMAGE_NAME=$1
+
+# Make build context
+echo "Making build context..."
+mkdir build_context
+echo "Add hbase at ${HBASE_HOME} to build context"
+cp -r $HBASE_HOME ./build_context/hbase
+echo "Add hadoop at ${HADOOP_HOME} to build context"
+cp -r $HADOOP_HOME ./build_context/hadoop
+echo "Add hive at ${HIVE_HOME} to build context"
+cp -r $HIVE_HOME ./build_context/hive
+echo "Add sentry at ${SENTRY_HOME} to build context"
+cp -r $SENTRY_HOME ./build_context/sentry
+echo "Add ranger at ${RANGER_HOME} to build context"
+cp -r $RANGER_HOME ./build_context/ranger
+echo "Add hadoop-lzo at ${HADOOP_LZO} to build context"
+cp -r $HADOOP_LZO ./build_context/hadoop-lzo
+
+echo "Prepare JDBC Driver from ${POSTGRES_JDBC_DRIVER} to build context"
+cp ${POSTGRES_JDBC_DRIVER} ./build_context/hive/lib/
+
+docker build -t $IMAGE_NAME --build-arg CONTEXT_DIR=./build_context .
+
+echo "Cleanup build context"
+rm -rf ./build_context
+
+echo "Done. The image is built with name ${IMAGE_NAME}."
